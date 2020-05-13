a2f781ac74f3a0afca3ed2785c18740c07298466
Mohit Sethi <mohitsethi.32@gmail.com>
2020-05-13 20:09:03 +0530

    Added events to ImGui layer.

--- RushEngine/src/Rush/ImGui/ImGuiLayer.h
+++ RushEngine/src/Rush/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Rush/Layer.h"
 
+#include "Rush/Events/ApplicationEvent.h"
+#include "Rush/Events/KeyEvent.h"
+#include "Rush/Events/MouseEvent.h"
+
 namespace Rush
 {
 	class RUSH_API ImGuiLayer : public Layer
@@ -15,6 +19,15 @@ namespace Rush
 		void OnUpdate();
 		void OnEvent(Event& event);
 	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 		float m_Time = 0.0f;
 	};
 }
