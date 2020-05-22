caf688c0912fec5a7d262adcd4134ce27bad9bd3
Mohit Sethi <mohitsethi.32@gmail.com>
2020-05-22 19:39:16 +0530

    Changed macro name

--- RushEngine/src/Rush/Log.h
+++ RushEngine/src/Rush/Log.h
@@ -20,15 +20,15 @@ namespace Rush
 }
 
 // Core log macros
-#define RS_CORE_ERROR(...)    ::Rush::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define RS_CORE_WARN(...)     ::Rush::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define RS_CORE_INFO(...)     ::Rush::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define RS_CORE_TRACE(...)    ::Rush::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define RS_CORE_FATAL(...)    ::Rush::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define RS_CORE_ERROR(...)		::Rush::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define RS_CORE_WARN(...)		::Rush::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define RS_CORE_INFO(...)		::Rush::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define RS_CORE_TRACE(...)		::Rush::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define RS_CORE_CRITICAL(...)	::Rush::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log errors
-#define RS_ERROR(...)         ::Rush::Log::GetClientLogger()->error(__VA_ARGS__)
-#define RS_WARN(...)          ::Rush::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define RS_INFO(...)          ::Rush::Log::GetClientLogger()->info(__VA_ARGS__)
-#define RS_TRACE(...)	      ::Rush::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define RS_FATAL(...)         ::Rush::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define RS_ERROR(...)		::Rush::Log::GetClientLogger()->error(__VA_ARGS__)
+#define RS_WARN(...)		::Rush::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define RS_INFO(...)		::Rush::Log::GetClientLogger()->info(__VA_ARGS__)
+#define RS_TRACE(...)		::Rush::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define RS_CRITICAL(...)	::Rush::Log::GetClientLogger()->critical(__VA_ARGS__)
