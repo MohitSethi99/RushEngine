cc5e3e62a1cf073a5f08b7044de7466b7acae4a4
Mohit Sethi <mohitsethi.32@gmail.com>
2020-05-23 21:57:17 +0530

    Added missing Sandbox.cpp

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -4,26 +4,157 @@ class ExampleLayer : public Rush::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example")
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0.0f)
 	{
+		m_VertexArray.reset(Rush::VertexArray::Create());
+
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
+		};
+
+		std::shared_ptr<Rush::VertexBuffer> vertexBuffer;
+		vertexBuffer.reset(Rush::VertexBuffer::Create(vertices, sizeof(vertices)));
+		Rush::BufferLayout layout = {
+			{ Rush::ShaderDataType::Float3, "a_Position" },
+			{ Rush::ShaderDataType::Float4, "a_Color" }
+		};
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		uint32_t indices[3] = { 0, 1, 2 };
+		std::shared_ptr<Rush::IndexBuffer> indexBuffer;
+		indexBuffer.reset(Rush::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		m_SquareVA.reset(Rush::VertexArray::Create());
+
+		float squareVertices[3 * 4] = {
+			-0.75f, -0.75f, 0.0f,
+			 0.75f, -0.75f, 0.0f,
+			 0.75f,  0.75f, 0.0f,
+			-0.75f,  0.75f, 0.0f
+		};
+
+		std::shared_ptr<Rush::VertexBuffer> squareVB;
+		squareVB.reset(Rush::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+		squareVB->SetLayout({
+			{ Rush::ShaderDataType::Float3, "a_Position" }
+			});
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+		std::shared_ptr<Rush::IndexBuffer> squareIB;
+		squareIB.reset(Rush::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+			uniform mat4 u_ViewProjection;
+			out vec3 v_Position;
+			out vec4 v_Color;
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+			in vec4 v_Color;
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
+		)";
+
+		m_Shader.reset(new Rush::Shader(vertexSrc, fragmentSrc));
+
+		std::string blueShaderVertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			uniform mat4 u_ViewProjection;
+			out vec3 v_Position;
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string blueShaderFragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+			void main()
+			{
+				color = vec4(0.2, 0.3, 0.8, 1.0);
+			}
+		)";
+
+		m_BlueShader.reset(new Rush::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 	}
 
-	void OnUpdate() override
+	void OnUpdate(Rush::Timestep ts) override
 	{
-		if (Rush::Input::IsKeyPressed(RS_KEY_TAB))
-			RS_TRACE("Tab key is pressed (poll)!");
+		if (Rush::Input::IsKeyPressed(RS_KEY_LEFT))
+			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
+		else if (Rush::Input::IsKeyPressed(RS_KEY_RIGHT))
+			m_CameraPosition.x += m_CameraMoveSpeed * ts;
+
+		if (Rush::Input::IsKeyPressed(RS_KEY_UP))
+			m_CameraPosition.y += m_CameraMoveSpeed * ts;
+		else if (Rush::Input::IsKeyPressed(RS_KEY_DOWN))
+			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
+
+		if (Rush::Input::IsKeyPressed(RS_KEY_A))
+			m_CameraRotation += m_CameraRotationSpeed * ts;
+		if (Rush::Input::IsKeyPressed(RS_KEY_D))
+			m_CameraRotation -= m_CameraRotationSpeed * ts;
+
+		Rush::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		Rush::RenderCommand::Clear();
+
+		m_Camera.SetPosition(m_CameraPosition);
+		m_Camera.SetRotation(m_CameraRotation);
+
+		Rush::Renderer::BeginScene(m_Camera);
+
+		Rush::Renderer::Submit(m_BlueShader, m_SquareVA);
+		Rush::Renderer::Submit(m_Shader, m_VertexArray);
+
+		Rush::Renderer::EndScene();
 	}
 
 	void OnEvent(Rush::Event& event) override
 	{
-		if (event.GetEventType() == Rush::EventType::KeyPressed)
-		{
-			Rush::KeyPressedEvent& e = (Rush::KeyPressedEvent&)event;
-			if (e.GetKeyCode() == RS_KEY_TAB)
-				RS_TRACE("Tab key is pressed (event)!");
-			RS_TRACE("{0}", (char)e.GetKeyCode());
-		}
 	}
+
+	private:
+		std::shared_ptr<Rush::Shader> m_Shader;
+		std::shared_ptr<Rush::VertexArray> m_VertexArray;
+
+		std::shared_ptr<Rush::Shader> m_BlueShader;
+		std::shared_ptr<Rush::VertexArray> m_SquareVA;
+
+		Rush::OrthographicCamera m_Camera;
+		glm::vec3 m_CameraPosition;
+		float m_CameraMoveSpeed = 5.0f;
+
+		float m_CameraRotation = 0.0f;
+		float m_CameraRotationSpeed = 180.0f;
 };
 
 class Sandbox : public Rush::Application
