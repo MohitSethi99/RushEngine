adde8d27fb5641994b42915a38602ba3f56270d0
Mohit Sethi <mohitsethi.32@gmail.com>
2020-05-23 22:28:34 +0530

    Added transforms to Renderer.

--- RushEngine/src/Rush/Window.h
+++ RushEngine/src/Rush/Window.h
@@ -14,8 +14,8 @@ namespace Rush
 		unsigned int Height;
 
 		WindowProps(const std::string& title = "Rush Engine",
-			unsigned int width = 1280,
-			unsigned int height = 720)
+			unsigned int width = 1920,
+			unsigned int height = 1080)
 			: Title(title), Width(width), Height(height)
 		{
 		}
