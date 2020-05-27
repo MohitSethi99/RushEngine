7856b8cb8a9dbf0c8c7ed5ad6aa34bd436f16319
Mohit Sethi <mohitsethi.32@gmail.com>
2020-05-27 21:50:29 +0530

    Added Rush::Scope and Rush::Ref definitions.

--- RushEngine/src/Rush/Core.h
+++ RushEngine/src/Rush/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #ifdef RS_PLATFORM_WINDOWS
 	#if HZ_DYNAMIC_LINK
 		#ifdef RS_BUILD_DLL
@@ -29,3 +31,12 @@
 #define BIT(x) (1 << x)
 
 #define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1) 
+
+namespace Rush
+{
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+}
